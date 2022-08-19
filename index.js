64390671dc53fe66c744e8bb69d49195a43aaa60
Shaun Yetman <57810567+syetman@users.noreply.github.com>
2022-08-19 15:44:18 -0400

    Created functions to add to homescore

---
+++ index.js
@@ -0,0 +1,17 @@
+let homeScore = 0
+let guestScore = 0
+
+function addOne(){
+    console.log("One added")
+    homeScore += 1
+}
+
+function addTwo(){
+    console.log("Two added")
+    homeScore += 2
+}
+
+function addThree(){
+    console.log("Three added")
+    homeScore += 3
+}
