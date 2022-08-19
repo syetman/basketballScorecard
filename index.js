48577ac5f09ba33a947473e6c1124e6d9183514d
Shaun Yetman <57810567+syetman@users.noreply.github.com>
2022-08-19 16:06:14 -0400

    Guest and home functions complete and functional

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-document.getElementById("home-score-display").textContent = homeScore
+let homeScore = 0
+let homeScoreDisplay = document.getElementById("home-score-display")
 
 let guestScore = 0
+let guestScoreDisplay = document.getElementById("guest-score-display")
 
-function addOne(){
+// Home Functions
+function homeAddOne(){
     console.log("One added")
     homeScore += 1
+    homeScoreDisplay.textContent = homeScore
 }
 
-function addTwo(){
+function homeAddTwo(){
     console.log("Two added")
     homeScore += 2
+    homeScoreDisplay.textContent = homeScore
+
 }
 
-function addThree(){
+function homeAddThree(){
     console.log("Three added")
     homeScore += 3
+    homeScoreDisplay.textContent = homeScore
 }
+
+// Guest Functions
+
+function guestAddOne(){
+    console.log("One added")
+    guestScore += 1
+    guestScoreDisplay.textContent = guestScore
+}
+
+function guestAddTwo(){
+    console.log("Two added")
+    guestScore += 2
+    guestScoreDisplay.textContent = guestScore
+
+}
+
+function guestAddThree(){
+    console.log("Three added")
+    guestScore += 3
+    guestScoreDisplay.textContent = guestScore
+}
+
+
